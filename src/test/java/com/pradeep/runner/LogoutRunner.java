5495e7133644237203af20a0d60be736bff848e5
Pradeep Kumar H N <prdp4747@gmail.com>
2024-07-02 00:51:26 +0530

    logout runner added

---
+++ src/test/java/com/pradeep/runner/LogoutRunner.java
@@ -0,0 +1,23 @@
+package com.pradeep.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.junit.runner.RunWith;
+import org.testng.annotations.DataProvider;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"com.pradeep.stepdefinitions"},
+        tags = "@Logout",
+        plugin = {"pretty", "html:target/html/cucumber-reports-Logout.html", "json:target/json/cucumber-Logout.json"},
+        monochrome = true
+)
+public class LogoutRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = false)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
